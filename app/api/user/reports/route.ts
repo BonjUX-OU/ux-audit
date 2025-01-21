3e10cd5e6cbe71945cd5b60be4aecde7f34b6fdd
bashybaranaba <bashybaranaba@gmail.com>
2025-01-21 20:12:44 +0300

    fix: set revalidate=0

--- app/api/user/reports/route.ts
+++ app/api/user/reports/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import Report from "@/models/Report";
 
+export const revalidate = 0;
 export async function GET(request: Request) {
   try {
     await dbConnect();
