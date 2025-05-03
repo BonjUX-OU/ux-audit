92da3865a1b841e1bc65eff92ebcf0003483e059
okaraca <omerkaraca.ko@gmail.com>
2025-05-03 22:08:38 +0300

    text changed.

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -71,7 +71,7 @@ function LoginPage() {
               </div>
 
               <h2 className="text-2xl font-bold mb-4">
-                Thank you Vercel👋
+                Thank you Kmras👋
               </h2>
               <p className="text-gray-600 max-w-md">
                 We are here to identify usability issues and opportunities,
