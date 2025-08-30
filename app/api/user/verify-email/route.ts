cb25d4ac0f9317944216dc1853107dc2da25f991
okaraca <omerkaraca.ko@gmail.com>
2025-08-30 17:57:43 +0300

    create stripe session

--- app/api/user/verify-email/route.ts
+++ app/api/user/verify-email/route.ts
@@ -2,27 +2,62 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import User from "@/models/User";
-import { signIn } from "next-auth/react";
 import { cookies } from "next/headers";
 import { encode } from "next-auth/jwt";
 import Report from "@/models/Report";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-03-31.basil", // or latest supported version
+});
 
 export async function POST(request: Request) {
   try {
-    const { token } = await request.json();
     const cookieStore = await cookies();
     const hasSession = cookieStore.get("register_session")?.value;
 
-     // this is reportId value send to customers for preview
-     const registerReportRef = cookieStore.get("register_ref")?.value; // 68b1fabf17cd199ed7137d26  
+    // this is reportId value send to customers for preview
+    const registerReportRef = cookieStore.get("register_ref")?.value; // 68b1fabf17cd199ed7137d26
+    if (registerReportRef) {
+      const report = await Report.findOne({ _id: registerReportRef });
+      // if customer come to register page, registered and want to verify email, we have to decide where to redirect them after successfully verified their email
+      if (report && !report.isPaid) {
+        // Redirect to payment page.
+        // Get Stripe Checkout session link from backend.
 
-     //get the reportById 
-     // check its already paid and assigned to a customer.
+        const session = await stripe.checkout.sessions.create({
+          line_items: [
+            {
+              price: "0.1", // <-- Stripe Price ID
+              quantity: 1,
+            },
+          ],
+          mode: "payment",
+          success_url: `${process.env.NEXT_PUBLIC_APP_URL}/success_stripe/${registerReportRef}`,
+          cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/cancel`,
+          metadata: {
+            registerReportRef,
+            userId: "to-be-updated-after-payment-success"
+          },
+        });
 
-     const report = await Report.findOne({ _id: registerReportRef });
+        session.url ? await VerifyCustomerEmailAndRedirect(request, hasSession, session.url) : null;
+                return NextResponse.redirect(session.url!);
 
-     if(report && report.paid && report.assignedToEmail){}
+      } else {
+        // Redirect to Dashboard.
+        await VerifyCustomerEmailAndRedirect(request, hasSession, "/dashboard");
+      }
+    }
+  } catch (error: any) {
+    console.error("Error fetching :", error.message);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
 
+async function VerifyCustomerEmailAndRedirect(request: Request, hasSession: string | undefined, redirectUrl: string) {
+  try {
+    const { token } = await request.json();
     await dbConnect();
 
     const twoDaysAgo = new Date(Date.now() - 48 * 60 * 60 * 1000);
@@ -55,8 +90,10 @@ export async function POST(request: Request) {
         secure: process.env.NODE_ENV === "production",
         maxAge: 60 * 60 * 24 * 30,
       });
-
+      
+      if(redirectUrl.includes("dashboard")){
       return NextResponse.redirect(new URL("/dashboard", process.env.NEXT_PUBLIC_APP_URL));
+      }
     }
   } catch (error: any) {
     console.error("Error fetching :", error.message);
